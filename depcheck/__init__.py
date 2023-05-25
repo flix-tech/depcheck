55db84b99a4df4bd4882d80c229e717e8ff0b373
Alexandru Marcu <alexandru.marcu@flixbus.com>
2023-05-25 15:54:59 +0200

    refactor: Move entrypoint into the package `__init__` module
    Remove the `main.py` module and relocate most of the content into the
    package initialization module. This also include a minor refactoring
    of the argument parser creation function.

---
+++ depcheck/__init__.py
@@ -0,0 +1,37 @@
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+
+from depcheck.depchecker import DepChecker
+from depcheck.models import DependencyReport, Ruleset
+from depcheck.readers import DependencyReader, RulesetReader
+
+DEFAULT_CONFIG_FILE_PATH = ".depcheck.yml"
+
+
+def run_depcheck() -> None:
+    command_args = create_argument_parser().parse_args()
+    arg_config_file = command_args.file
+    arg_root_package = command_args.root_package
+
+    print(f"[DepCheck] Reading ruleset from '{arg_config_file}'")
+    ruleset: Ruleset = RulesetReader(arg_config_file).read()
+    print(f"[DepCheck] Reading project packages from root package: '{arg_root_package}'")
+    dependency_report: DependencyReport = DependencyReader(ruleset, arg_root_package).read()
+    print("[DepCheck] Checking dependencies")
+    DepChecker(ruleset, dependency_report).run()
+
+
+def create_argument_parser() -> ArgumentParser:
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("root_package", help="Root package of the project")
+    parser.add_argument(
+        "-f",
+        "--file",
+        default=DEFAULT_CONFIG_FILE_PATH,
+        help="Path to the config file that includes layers and rules.",
+    )
+
+    return parser
+
+
+run_depcheck() if __name__ == "__main__" else None
